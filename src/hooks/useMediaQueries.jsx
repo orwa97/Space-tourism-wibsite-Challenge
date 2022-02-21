ae9b9ac45cd90c0afe6f5bd0632d1f82e4f05b03
orwa97 <orwa.aloufi@gmail.com>
2022-02-21 16:06:29 +0200

    defining MediaQ

--- src/hooks/useMediaQueries.jsx
+++ src/hooks/useMediaQueries.jsx
@@ -1,4 +1,11 @@
 import { useMediaQuery } from "react-responsive";
-const useMediaQueries = () => {};
+const useMediaQueries = () => {
+  const isBigScreen = useMediaQuery({ minWidth: 1281 });
+  const isDesktop = useMediaQuery({ minWidth: 1025 });
+  const istablet = useMediaQuery({ minWidth: 641 });
+  const isSmartPhone = useMediaQuery({ minWidth: 320 });
+
+  return { isBigScreen, isDesktop, istablet, isSmartPhone };
+};
 
 export default useMediaQueries;
