9cfa6d91a634b294a362f90560dd14abac767b8c
orwa97 <orwa.aloufi@gmail.com>
2022-02-23 23:10:43 +0200

    complete tablet's mediaQ

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import SubLayout from "../../components/subLayout/SubLayout";
+import useMediaQueries from "../../hooks/useMediaQueries";
 import ExploreBtn from "./exploreBtn/ExploreBtn";
 import classes from "./Home.module.scss";
 // import background from "/assets/home/bg-desktop.jpg";
 const Home = (props) => {
   const navigate = useNavigate();
+  const mediaQuery = useMediaQueries();
+
   const onClickHander = () => {
     navigate("/destination");
   };
+
   return (
     <SubLayout
-      className={classes.subLayout}
+      className={`${classes.subLayout} ${classes[mediaQuery]}`}
       leftHalf={
         <>
           <h5>So, you want to travel to</h5>
@@ -23,24 +27,11 @@ const Home = (props) => {
           </p>
         </>
       }
-      rightHalf={<ExploreBtn onClick={onClickHander} />}
+      rightHalf={
+        <ExploreBtn onClick={onClickHander} className={classes.expBtn} />
+      }
       background="/assets/home/bg-desktop.jpg"
     />
-    // <div className={classes.container}>
-    //   <div className={classes.box}>
-    //     <div className={classes.intro}>
-    //       <h5>So, you want to travel to</h5>
-    //       <h1>SPACE</h1>
-    //       <p>
-    //         Let’s face it; if you want to go to space, you might as well
-    //         genuinely go to outer space and not hover kind of on the edge of it.
-    //         Well sit back, and relax because we’ll give you a truly out of this
-    //         world experience!
-    //       </p>
-    //     </div>
-    //     <ExploreBtn onClick={onClickHander} />
-    //   </div>
-    // </div>
   );
 };
 
--- src/pages/home/exploreBtn/ExploreBtn.jsx
+++ src/pages/home/exploreBtn/ExploreBtn.jsx
@@ -2,8 +2,7 @@ import classes from "./ExploreBtn.module.scss";
 
 const ExploreBtn = (props) => {
   return (
-    <div className={classes.container}>
-      {/* <span className={classes.outerRing} id="outerRing" />x */}
+    <div className={`${classes.container} ${props.className}`}>
       <button className={classes.btn} onClick={props.onClick}>
         <h4>EXPLORE</h4>
       </button>
